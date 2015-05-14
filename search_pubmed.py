28727f66238e9f14da396bc5835d233e914f336b
lmandres <lmandres@users.noreply.github.com>
2015-05-14 16:28:02 -0400

    Update search_pubmed.py
    Added line to output license terms.

--- search_pubmed.py
+++ search_pubmed.py
@@ -7,6 +7,8 @@ import searchlib.eutils
 import searchlib.helper
 import searchlib.pubmeddb
 
+import sys
+
 class PubMedSearchApp:
     
     search_settings = None
@@ -214,5 +216,30 @@ class PubMedSearchApp:
         
 if __name__ == '__main__':
     
+    if sys.argv[1] == '--license':
+        print('''
+    PubMed Publications Search
+    Script that creates automatic searches based on author names, grant numbers, or organizations.
+    Copyright (C) 2015  Leo Andres
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Leo Andres
+    P. O. Box 3174
+    Decatur, GA 30031
+    lmandres@yahoo.com
+''')
+
     pubmed_search = PubMedSearchApp()
     pubmed_search.run_pubmed_search()
